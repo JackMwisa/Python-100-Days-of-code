c3d3e92f3405a08dfcd3a059f166d637d94d7555
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2023-11-21 12:10:17 +0300

    Update Hello.py

--- Day 1/Hello.py
+++ Day 1/Hello.py
@@ -1,24 +1,29 @@
 
-print(4+5)
-print(4-5)
-print(4/5)
-print(4*5)
-print(4**5)
+# print(4+5)
+# print(4-5)
+# print(4/5)
+# print(4*5)
+# print(4**5)
 
 
-print(type(10))          # Int
-print(type(3.14))        # Float
-print(type(1 + 3j))      # Complex number
-print(type('Asabeneh'))  # String
-print(type([1, 2, 3]))   # List
-print(type({'name':'Asabeneh'})) # Dictionary
-print(type({9.8, 3.14, 2.7}))    # Set
-print(type((9.8, 3.14, 2.7)))    # Tuple
+# print(type(10))          # Int
+# print(type(3.14))        # Float
+# print(type(1 + 3j))      # Complex number
+# print(type('Asabeneh'))  # String
+# print(type([1, 2, 3]))   # List
+# print(type({'name':'Asabeneh'})) # Dictionary
+# print(type({9.8, 3.14, 2.7}))    # Set
+# print(type((9.8, 3.14, 2.7)))    # Tuple
 
-print(2 + 3)             # addition(+)
-print(3 - 1)             # subtraction(-)
-print(2 * 3)             # multiplication(*)
-print(3 / 2)             # division(/)
-print(3 ** 2)            # exponential(**)
-print(3 % 2)             # modulus(%)
-print(3 // 2)            # Floor division operator(//)
+# print(2 + 3)             # addition(+)
+# print(3 - 1)             # subtraction(-)
+# print(2 * 3)             # multiplication(*)
+# print(3 / 2)             # division(/)
+# print(3 ** 2)            # exponential(**)
+# print(3 % 2)             # modulus(%)
+# print(3 // 2)            # Floor division operator(//)
+
+
+names = "Jack Mwisa"
+
+print(names)
