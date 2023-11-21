9c5af97d7a7bc3579ddf60ec7a37283b79b44d5b
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2023-11-21 20:23:06 +0300

    .

---
+++ Day 1/exe3.py
@@ -0,0 +1,6 @@
+type_str = type('67')
+type_int = type(67)
+print("Type of '67':", type_str)
+print("Type of 67:", type_int)
+
+
---
+++ Day 1/exercise2.py
@@ -0,0 +1,8 @@
+
+num1 = 5
+num2 = 3
+
+product = num1 * num2  # Multiplication using *
+power = num1 ** num2  # Exponentiation using **
+print("Product:", product)
+print("Power:", power)
