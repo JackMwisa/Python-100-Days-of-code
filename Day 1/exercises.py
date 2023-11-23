e6b54b20f82292a1c9c14214ee9e44c9ed1e87f9
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2023-11-23 13:22:06 +0300

    .

--- Day 1/exercises.py
+++ Day 1/exercises.py
@@ -1,7 +1,57 @@
+# # This is the first comment
+# # This is the second comment
+# # Python is eating the world
+# #jkdjksjkjskdjksjdk
+
+# """_summary_
+#     sl,dlsd
+#     sldksldk
+#     ;sd;sld;sld;
+    
+# """
 
 
 
-name = "Neema Kassanova"
-# print(name)  # Correct syntax
-# print(name')  # Missing closing quotation mark
-print("name")  # Missing opening quotation mark
+# # age = 89
+# # print(age)
+
+
+
+# print('Asabeneh')
+# print("Finland")
+# print('Python')
+# print('I love teaching')
+# print('I hope you are enjoying the first day of 30DaysOfPython Challenge')
+
+# print('Asabeneh', 'Finland', 'Python')
+# print('Asabeneh', 'Finland', 'Python', sep='-')
+
+# # print(True)  #  Is the light on? If it is on, then the value is True
+# # print(False) # Is the light on? If it is off, then the value is False
+
+# jackkk =[0, 1, 2, 3, 4, 5]
+# print(jackkk)
+
+# print(2 + 3)             # addition(+)
+# print(3 - 1)             # subtraction(-)
+# print(2 * 3)             # multiplication(*)
+# print(3 / 2)             # division(/)
+# print(3 ** 2)            # exponential(**)
+# print(3 % 2)             # modulus(%)
+# print(3 // 2)            # Floor division operator(//)
+
+# print(type(10000))
+# print(type("10000"))
+
+# Nokk = "jkjk"
+# _jkjk = "jksdj"
+# 78kkj
+
+# jack = "Stop"
+# Jack = "Sff"
+# birth_year = 89989
+
+
+
+
+
