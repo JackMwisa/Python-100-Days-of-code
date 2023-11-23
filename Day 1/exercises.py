1768625b2a804756ffd3152561c24307074f9615
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2023-11-23 17:15:00 +0300

    Update exercises.py

--- Day 1/exercises.py
+++ Day 1/exercises.py
@@ -55,6 +55,8 @@
 
 
 first_name = 'Asabeneh'
+
+first_name = 'Jack'
 last_name = 'Yetayeh'
 country = 'Finland'
 city = 'Helsinki'
@@ -81,3 +83,29 @@ print('Married: ', is_married)
 print('Skills: ', skills)
 print('Person information: ', person_info)
 
+# int to float
+num_int = 10
+print('num_int',num_int)         # 10
+num_float = float(num_int)
+print('num_float:', num_float)   # 10.0
+
+# float to int
+gravity = 9.81
+print(int(gravity))             # 9
+
+# int to str
+num_int = 10
+print(num_int)                  # 10
+num_str = str(num_int)
+print(num_str)                  # '10'
+
+# str to int or float
+num_str = '10.6'
+print('num_int', int(num_str))      # 10
+print('num_float', float(num_str))  # 10.6
+
+# str to list
+first_name = 'Asabeneh'
+print(first_name)               # 'Asabeneh'
+first_name_to_list = list(first_name)
+print(first_name_to_list)            # ['A', 's', 'a', 'b', 'e', 'n', 'e', 'h']
