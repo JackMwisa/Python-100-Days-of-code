790685e07b5d7df0bb02370942f701efe5598b66
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2023-11-24 18:42:38 +0300

    Create day-1 chal.py

---
+++ Day 1/day-1 chal.py
@@ -0,0 +1,7 @@
+
+
+
+print("Day 1 - string Manipulation")
+print('String concatenation is done with the "+" sign')
+print('e.g. print("Hello " + "World")')
+print("New line can be created with backslash and n\n")
