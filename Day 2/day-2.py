c11f961cce84d3a634e19fa003fb1aacd9ca8b17
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2023-11-26 15:33:57 +0300

    datatypes

--- Day 2/day-2.py
+++ Day 2/day-2.py
@@ -1,4 +1,33 @@
-print("Yes")
+# print("Yes")
 
 
-print(len(1232))
+# print(len(str(1232)))
+
+# Data types in python
+
+# Strings
+name = "Jack"
+names = 'Jack Mwisa'
+
+print(name)
+print(names)
+print(names[0])
+
+# print(123)
+print("12")
+
+print("1223" + "1223")
+
+
+# Intergers
+
+print(123 + 123)
+
+# Large integers
+
+print(923_575_454)
+
+# float
+print(123.9)
+print(1.5 * 2.08)
+
