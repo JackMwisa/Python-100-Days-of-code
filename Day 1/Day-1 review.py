7f1db487bd5d97860d91290098e9b5b9bbb67489
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2023-11-28 15:12:14 +0300

    Update Day-1 review.py

--- Day 1/Day-1 review.py
+++ Day 1/Day-1 review.py
@@ -1,5 +1,11 @@
 
 
+# print(6)
+# print(6,6,6,6,6)
+# print("Jack", "james", 89, "23454")
+
+
+
 
 
 
@@ -7,8 +13,14 @@
 number_one = 10
 number_two = 5
 
+# Code
+number_three = number_one
+number_one = number_two
+number_two = number_three 
 
 
-print("First number = " + number_one)
-print("second number = " + number_two)
+print("First number = ",  number_one)
+print("second number = ",  number_two)
 
+# First number = 5
+# Second number = 10
