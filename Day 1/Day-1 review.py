66150422a4bb9c0062706ccfa224bc74ac012716
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2023-11-28 15:20:33 +0300

    Update Day-1 review.py

--- Day 1/Day-1 review.py
+++ Day 1/Day-1 review.py
@@ -1,8 +1,8 @@
 
 
-# print(6)
-# print(6,6,6,6,6)
-# print("Jack", "james", 89, "23454")
+# # print(6)
+# # print(6,6,6,6,6)
+# # print("Jack", "james", 89, "23454")
 
 
 
@@ -10,19 +10,43 @@
 
 
 
-number_one = 10
-number_two = 5
+# number_one = 10
+# number_two = 5
 
-# Code
-number_three = number_one
-number_one = number_two
-number_two = number_three 
+# # Code
+# number_three = number_one
+# number_one = number_two
+# number_two = number_three 
 
 
-print("First number = ",  number_one)
-print("second number = ",  number_two)
+# print("First number = ",  number_one)
+# print("second number = ",  number_two)
 
-# First number = 5
-# Second number = 10
+# # First number = 5
+# # Second number = 10
 
+
+
+# name = input("What is your name? ")
+# print(name)
+
+number1 = input("Enter a number: ") 
+number2 = input("Enter another number: ")
+
+print(type(number1))
+print(type(number2))
+
+# int()
+# float()
+#  str()
+
+number1 = int(number1)
+number2 = int(number2)
+
+print(type(number1))
+print(type(number2))
+
+
+# print(number1)
+# print(number2)
 
