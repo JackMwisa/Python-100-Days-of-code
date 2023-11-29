5a76e4072d187562d1b8263d733a64490f9e738d
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2023-11-30 02:36:39 +0300

    Update Day-3.py

---
+++ Day 3/Day-3.py
@@ -0,0 +1,11 @@
+
+
+import math
+
+def print_circum(radius):
+  circumference = 2 * math.pi * radius
+  print("Circumference:", circumference)
+
+print_circum(5)
+print_circum(10)
+print_circum(15)
