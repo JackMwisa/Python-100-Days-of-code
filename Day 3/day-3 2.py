3e18745b8b3078cf16b5e2fffcb9c245e556648e
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2023-11-30 17:05:52 +0300

    Update day-3 2.py

--- Day 3/day-3 2.py
+++ Day 3/day-3 2.py
@@ -1,42 +1,36 @@
-def calculate_price(items, item_prices, combo_discounts=None, gift_pack_discount=0.25):
-    if not items:
-        return 0
+def generate_catalog():
+    items = {
+        "Item 1": 200.0,
+        "Item 2": 400.0,
+        "Item 3": 600.0
+    }
 
-    head, *tail = items
-    price = item_prices[head]
+    catalog = []
 
-    if not tail:
-        return price
+    # Add individual items to the catalog
+    catalog.extend(map(lambda x: (x[0], x[1]), items.items()))
 
-    combo_pack_discount = combo_discounts.get(
-        f"Combo 1(Item 1 + 2)" if head == "Item 2" and tail[0] == "Item 1" else None
-    )
-    if combo_pack_discount is not None:
-        price *= (1 - combo_pack_discount)
+    # Add combo packs with a 10% discount
+    combos = [("Combo 1(Item 1 + 2)", ["Item 1", "Item 2"]),
+              ("Combo 2(Item 2 + 3)", ["Item 2", "Item 3"]),
+              ("Combo 3(Item 1 + 3)", ["Item 1", "Item 3"])]
 
-    return price + calculate_price(tail, item_prices, combo_discounts, gift_pack_discount)
+    catalog.extend(map(lambda x: (x[0], sum(items[item] for item in x[1]) * 0.9), combos))
 
+    # Add gift pack with a 25% discount
+    gift_pack_items = ["Item 1", "Item 2", "Item 3"]
+    gift_pack_price = sum(items[item] for item in gift_pack_items)
+    catalog.append(("Gift Pack(Item 1 + 2 + 3)", gift_pack_price * 0.75))  # 25% discount
 
-# Example usage
-item_prices = {
-    "Item 1": 200.0,
-    "Item 2": 150.0,
-    "Item 3": 300.0,
-    "Combo 1(Item 1 + 2)": 320.0,
-    "Gift Pack": 825.0,
-}
+    # Generate the catalog output
+    output = "Online store\n" + "-" * 27 + "\n"
+    output += "{:<40} {:<10}\n".format("Product(s)", "Price")
 
-combo_discounts = {
-    "Combo 1(Item 1 + 2)": 0.15,
-    "Combo 2(Item 1 + 3)": 0.20,
-}
+    output += "\n".join(map(lambda x: "{:<40} {:<10.1f}".format(x[0], x[1]), catalog))
 
-# Calculate total price for Item 1 and Item 2 with default combo discount
-items = ["Item 1", "Item 2"]
-total_price = calculate_price(items, item_prices, combo_discounts)
-print(f"Total price for Item 1 and Item 2 with default combo discount: {total_price}")
+    output += "\n" + "_" * 27 + "\nFor delivery Contact:98764678899"
+    
+    print(output)
 
-# Calculate total price for Item 1 and Item 2 with custom combo discount
-items = ["Item 1", "Item 2"]
-total_price = calculate_price(items, item_prices, combo_discounts={"Combo 1(Item 1 + 2)": 0.25})
-print(f"Total price for Item 1 and Item 2 with custom combo discount: {total_price}")
+# Call the function to generate and print the catalog
+generate_catalog()
