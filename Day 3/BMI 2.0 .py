de59691f6918b3785360b0844657f5f350afdd49
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2023-12-03 17:31:27 +0300

    Update BMI 2.0 .py

--- Day 3/BMI 2.0 .py
+++ Day 3/BMI 2.0 .py
@@ -15,12 +15,12 @@ print(bmi)
 
 
 if bmi < 18.5:
-    print("underweight")
+    print(f"Your BMI is {bmi}, you're underweight")
 elif bmi < 25:
-    print("Normal weight")
+    print(f"Your BMI is {bmi}, you're normal weight")
 elif bmi < 30:
-    print("Overweight")
+    print(f"Your BMI is {bmi}, you're overweight")
 elif bmi < 35:
-    print("Obese")
+    print(f"Your BMI is {bmi}, you're obese")
 else:
-    print("Clinically obese")
+    print(f"Your BMI is {bmi}, you're clinically obese")
