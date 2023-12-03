cebc603bb8386f416fab4958af67981659f5b29c
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2023-12-03 09:34:41 +0300

    using f strings in python

--- Day 2/day-2 arth.py
+++ Day 2/day-2 arth.py
@@ -18,3 +18,12 @@ print(3 // 5)
 # print(3 * (3 + 3) / 3 - 3)
 
 print(round(67.090909, 2))
+
+
+score = 0
+height = 1.8
+is_winning = True
+
+# F strings
+print(f"Your score is {score}, your height is {height}, you are winning is {True}")
+
