7842915c744d5a6b0dd8db112145d0ac9bae5819
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2023-12-03 09:29:49 +0300

    round function

--- Day 2/day-2 arth.py
+++ Day 2/day-2 arth.py
@@ -1,11 +1,11 @@
 
 
-print(3 + 5)
+# print(3 + 5)
 print(3 - 5)
 print(3 ** 5)
 print(3 / 5)
 print(3 // 5)
-print(3 * 5)
+# print(3 * 5)
 
 # Order
 
@@ -15,4 +15,6 @@ print(3 * 5)
 # + and -
 
 
-print(3 * (3 + 3) / 3 - 3)
+# print(3 * (3 + 3) / 3 - 3)
+
+print(round(67.090909, 2))
