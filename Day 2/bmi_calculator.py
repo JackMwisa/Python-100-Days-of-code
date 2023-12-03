bbf1e85de5945e7f84014a35d51d9f81f935a578
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2023-12-03 09:21:18 +0300

    bmi

---
+++ Day 2/bmi_calculator.py
@@ -0,0 +1,13 @@
+
+
+height = input("Enter your height in m: ")
+
+weight = input("Enter your weight in kg: ")
+
+
+# bmi = weight(kg) / height ** 2 (m2)
+
+
+bmi = int(weight) / float(height) ** 2
+
+print(int(bmi))
