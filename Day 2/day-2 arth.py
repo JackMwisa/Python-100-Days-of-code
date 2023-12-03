b305d43de1df0e9f259ef2d8ce60bcb1052d2561
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2023-12-03 09:13:40 +0300

    Create day-2 arth.py

---
+++ Day 2/day-2 arth.py
@@ -0,0 +1,18 @@
+
+
+print(3 + 5)
+print(3 - 5)
+print(3 ** 5)
+print(3 / 5)
+print(3 // 5)
+print(3 * 5)
+
+# Order
+
+# ()
+# ** (exponentiation)
+# * and /
+# + and -
+
+
+print(3 * (3 + 3) / 3 - 3)
