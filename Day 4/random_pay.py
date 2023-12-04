c8a94df5f38f60688f5809bcdb66fbb32902fa01
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2023-12-04 20:53:25 +0300

    Update random_pay.py

--- Day 4/random_pay.py
+++ Day 4/random_pay.py
@@ -6,8 +6,9 @@ name_string = input("Give me everybody's name, separated by a comma. ")
 # Jack, Mark, Lily, Kevin, Sarah, Laura, Martin
 name = name_string.split(", ")
 
-payer = random.randint(0, 7)
+payer = random.randint(0, len(name))
 
+# print(len(name))
 
 bill_payer = f"The one who will pay the bill is {name[payer]}"
 
