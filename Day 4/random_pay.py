872b8dedce87bd7e65083ebc770ae67ea0adcebb
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2023-12-04 21:02:31 +0300

    .

--- Day 4/random_pay.py
+++ Day 4/random_pay.py
@@ -6,7 +6,13 @@ name_string = input("Give me everybody's name, separated by a comma. ")
 # Jack, Mark, Lily, Kevin, Sarah, Laura, Martin
 name = name_string.split(", ")
 
-payer = random.randint(0, len(name))
+len_of_list = len(name)
+
+payer = random.randint(0, len_of_list - 1)
+
+# ch = random.choice(name)
+
+# print(ch)
 
 # print(len(name))
 
