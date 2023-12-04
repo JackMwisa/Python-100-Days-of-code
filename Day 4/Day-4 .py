3e18b0c522b85d6bed688edb02c720ae40c37a46
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2023-12-04 20:00:52 +0300

    Update Day-4 .py

--- Day 4/Day-4 .py
+++ Day 4/Day-4 .py
@@ -3,6 +3,11 @@ import my_module
 
 random_integer = random.randint(0,10)
 
+# random float
+random_float = random.random() * 5
+
+
 print(random_integer)
+print(random_float)
 
 print(my_module.pi)
