b8de3253da0a99baad38d8ca860bec9812f4676e
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2023-12-06 12:41:19 +0300

    Update try.py

--- Day 4/try.py
+++ Day 4/try.py
@@ -1,42 +1,4 @@
 
-# import random
-
-# name_string = input("Give me everybody's name, separated by a comma. ")
-
-# # Jack, Mark, Lily, Kevin, Sarah, Laura, Martin
-# name = name_string.split(", ")
-
-# len_of_list = len(name)
-
-# payer = random.randint(0, len_of_list - 1)
-
-# # ch = random.choice(name)
-
-# # print(ch)
-
-# # print(len(name))
-
-# bill_payer = f"The one who will pay the bill is {name[payer]}"
-
-# print(bill_payer)
-
-# import random
-
-# name_string = input("Give me everybody's name, separated by a comma. ")
-
-# # Jack, Mark, Lily, Kevin, Sarah, Laura, Martin
-# name = name_string.split(", ")
-
-# len_of_list = len(name)
-
-# payer = random.randint(0, len_of_list - 1)
-
-# # ch = random.choice(name)
-
-# # print(ch)
-
-# # print(len(name))
-
 # bill_payer = f"The one who will pay the bill is {name[payer]}"
 
 # print(bill_payer)
