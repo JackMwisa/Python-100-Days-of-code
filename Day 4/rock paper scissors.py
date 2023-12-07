c369afdb2f35081c76501d06190be2d974479c5f
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2023-12-07 19:24:11 +0300

    Update rock paper scissors.py

--- Day 4/rock paper scissors.py
+++ Day 4/rock paper scissors.py
@@ -1,3 +1,5 @@
+import random
+
 rock = """
     _______
 ---'   ____)
@@ -25,4 +27,26 @@ scissors = """
 ---.__(___)
 """
 
-print(rock, paper, scissors)
+my_options = [rock, paper, scissors]
+
+choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 For Scissors.\n"))
+
+my_choice = my_options[choice]
+
+print(my_choice)
+
+computer_choice = my_options[random.randint(0,2)]
+ 
+print(f"computer choice \n {computer_choice}")
+
+
+if 
+
+
+
+
+
+
+
+
+# print(rock, paper, scissors)
