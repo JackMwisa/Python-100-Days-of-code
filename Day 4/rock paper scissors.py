62d73a87106730520f51e59f9a89b917bc0bcdd3
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2023-12-07 19:41:39 +0300

    Update rock paper scissors.py

--- Day 4/rock paper scissors.py
+++ Day 4/rock paper scissors.py
@@ -30,19 +30,27 @@ scissors = """
 my_options = [rock, paper, scissors]
 
 choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 For Scissors.\n"))
-
+# choice list
 my_choice = my_options[choice]
 
 print(my_choice)
 
+# random variable 
 random_cp = random.randint(0,2)
 
 computer_choice = my_options[random_cp]
  
+
+
+# print(computer_choice)
+# print(choice)
+# print(rock, paper, scissors)
+
 print(f"computer choice \n {computer_choice}")
 
-
-if choice == 0 and random_cp == 1:
+if choice > 2:
+    print("Wrong choice!")
+elif choice == 0 and random_cp == 1:
     print("You have failed")
 elif choice == 1 and random_cp == 2:
     print("You have failed")
@@ -57,9 +65,3 @@ else:
 
 
 
-
-# print(computer_choice)
-# print(choice)
-
-
-# print(rock, paper, scissors)
