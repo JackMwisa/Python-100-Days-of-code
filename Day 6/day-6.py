0ce5c425dd13ed910fd8718602416227d8f27036
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2023-12-15 01:29:28 +0300

    Update day-6.py

--- Day 6/day-6.py
+++ Day 6/day-6.py
@@ -1,17 +1,20 @@
-
-import math
-
-def hypotenuse(leg1, leg2):
-    sum_of_squares = leg1**2 + leg2**2
-    hypotenuse_length = math.sqrt(sum_of_squares)
-    return hypotenuse_length
+def calculate_factorial(n):
+    result = 1
+    
+    # Base case: factorial of 0 and 1 is 1
+    if n == 0 or n == 1:
+        return result
+    else:
+        # Recursive multiplication
+        result = n * calculate_factorial(n-1)
+        return result
 
 # Testing the Function
-result1 = hypotenuse(3, 4)
-result2 = hypotenuse(5, 12)
-result3 = hypotenuse(8, 15)
+result1 = calculate_factorial(5)
+result2 = calculate_factorial(0)
+result3 = calculate_factorial(3)
 
-# Displaying Results in Learning Journal
-print("Test 1: hypotenuse(3, 4) =", result1)
-print("Test 2: hypotenuse(5, 12) =", result2)
-print("Test 3: hypotenuse(8, 15) =", result3)
+# Displaying Results and Explanation
+print("Test 1: calculate_factorial(5) =", result1)
+print("Test 2: calculate_factorial(0) =", result2)
+print("Test 3: calculate_factorial(3) =", result3)
