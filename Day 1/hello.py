d59c3f453b6004c2df8168b12986e2a6c85c2d22
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2024-03-21 17:52:32 +0300

    Update hello.py

--- Day 1/hello.py
+++ Day 1/hello.py
@@ -1,1 +1,9 @@
-print('Hello world!\nHello world!\nHello world1')
+print('Hello world!\nHello world!\nHello world1\n')
+
+
+#string concatination
+
+print('Hello '+ 'jack')
+print('Hello'+ ' jack')
+print('Hello'+ " " +'jack')
+
