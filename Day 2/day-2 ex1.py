1624d7ee729df14af1b009f4d01f90b4d551cfa1
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2024-03-24 23:29:10 +0300

    Update day-2 ex1.py

--- Day 2/day-2 ex1.py
+++ Day 2/day-2 ex1.py
@@ -1,38 +1,38 @@
-# # print("Yes")
+# print("Yes")
 
 
-# # print(len(str(1232)))
+# print(len(str(1232)))
 
-# # Data types in python
+# Data types in python
 
-# # Strings
-# name = "Jack"
-# names = 'Jack Mwisa'
+# Strings
+name = "Jack"
+names = 'Jack Mwisa'
 
-# print(name)
-# print(names)
-# print(names[0])
+print(name)
+print(names)
+print(names[0])
 
-# # print(123)
-# print("12")
+# print(123)
+print("12")
 
-# print("1223" + "1223")
-
-
-# Intergers
-
-# print(123 + 123)
-
-# # Large integers
-
-# print(923_575_454)
-
-# # float
-# print(123.9)
-# print(1.5 * 2.08)
+print("1223" + "1223")
 
 
+Intergers
 
-# # boolean
-# print(True)
-# print(False)
+print(123 + 123)
+
+# Large integers
+
+print(923_575_454)
+
+# float
+print(123.9)
+print(1.5 * 2.08)
+
+
+
+# boolean
+print(True)
+print(False)
