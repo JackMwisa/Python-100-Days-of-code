376727372598fb9d97d06601df5759cd23513566
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2024-03-24 23:29:01 +0300

    Update day-2 chal1.py

--- Day 2/day-2 chal1.py
+++ Day 2/day-2 chal1.py
@@ -1,7 +1,7 @@
-# done two digit number 
-two_digit_number = input("Type your 2 digit number: ")
+# # done two digit number 
+# two_digit_number = input("Type your 2 digit number: ")
 
-num_one = int(two_digit_number[0])
-num_two = int(two_digit_number[1])
+# num_one = int(two_digit_number[0])
+# num_two = int(two_digit_number[1])
 
-print(num_one + num_two)cv
+# print(num_one + num_two)cv
