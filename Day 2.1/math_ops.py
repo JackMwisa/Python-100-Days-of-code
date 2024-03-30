e2692dc8faa51a4b9ec5ef2c9e3b6d77c031bce2
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2024-03-30 14:20:37 +0300

    Update math_ops.py

--- Day 2.1/math_ops.py
+++ Day 2.1/math_ops.py
@@ -16,3 +16,16 @@ print(14/5)
 print(14//5) # integer division (rounds down to the nearest whole
 
 #  5. modulus
+print(244%6)
+
+# 6. exponent
+print(9**3)
+
+# bitwise operations
+a = 0b1101
+b = 0b1010
+c = a+b
+
+print(a)
+print(b)
+print(c)
