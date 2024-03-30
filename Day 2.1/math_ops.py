d73d3eb5ce67bb9ba074c18b41adad5b27dce26c
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2024-03-30 14:18:10 +0300

    math ops

---
+++ Day 2.1/math_ops.py
@@ -0,0 +1,18 @@
+
+
+# arithmetics ops
+
+# 1. addition
+print(4+5)
+
+# 2. substraction
+print(3-5)
+
+# 3. multiplication
+print(24*5)
+
+# 4. Division
+print(14/5)
+print(14//5) # integer division (rounds down to the nearest whole
+
+#  5. modulus
--- Day 2.1/two_digit_number.py
+++ Day 2.1/two_digit_number.py
@@ -2,7 +2,11 @@
 
 two_digit_number = input("type a two digit number: ")
 
+# type keyword
+
 first_digit = int(two_digit_number[0])
 second_digit = int(two_digit_number[1])
 
-print(first_digit + second_digit)
+answer = first_digit + second_digit
+
+print(answer)
