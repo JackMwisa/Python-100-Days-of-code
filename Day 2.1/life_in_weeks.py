f88ce4c5223a631943db987cd25079a1113a9240
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2024-03-30 14:54:08 +0300

    Update life_in_weeks.py

--- Day 2.1/life_in_weeks.py
+++ Day 2.1/life_in_weeks.py
@@ -5,8 +5,8 @@ age = input("what is your current age? ")
 remaining_years = 90 - int(age) 
 
 days  = int(remaining_years * 365.2421897) # days
-weeks = int(remaining_years * 52)
-months = int(remaining_years * 12)
+weeks = remaining_years * 52
+months = remaining_years * 12
 
 print(f"You have {days} days, {weeks} weeks, and {months} months left.")
 # you have  x days x weeks x months left if you lived until 90
