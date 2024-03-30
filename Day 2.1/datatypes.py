49da7d6c9fab548e0ece59e5143d05b1fd665012
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2024-03-30 14:04:49 +0300

    Update datatypes.py

--- Day 2.1/datatypes.py
+++ Day 2.1/datatypes.py
@@ -1,9 +1,23 @@
 
 
-#Date types
+# Date types
 
-#Strings
+# Strings
 print("Hello"[2])  #subscripting
-
+print("Hello " + "world")
 print("345" + "123")
 
+# Integer
+
+print(123 + 324)
+print(234_234_875 + 432_245_867)
+
+# Float 
+
+pi = 3.1415
+print(pi)
+
+# Booleans
+
+print(True)
+print(False)
