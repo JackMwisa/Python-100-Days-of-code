6bcbcfaa78440610f473d0de17476e079e176061
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2024-04-02 11:29:10 +0300

    Update leap year challenge.py

--- Day 3/leap year challenge.py
+++ Day 3/leap year challenge.py
@@ -4,7 +4,7 @@
 year = int(input("Which year do you want to check? "))
 
 
-if year % 4 == 0 and (year % 100 == 0 and year % 400 == 0):
+if year % 4 == 0 or (year % 100 == 0 and year % 400 == 0):
     print(f"{year} is a leap year")
 else:
     print(f"{year} is not a leap year") 
