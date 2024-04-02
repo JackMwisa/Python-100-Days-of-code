e0bae3d754507a94bec457e9dc71583997ec04a4
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2024-04-02 10:39:59 +0300

    Update one.py

--- Day 3.1/one.py
+++ Day 3.1/one.py
@@ -4,5 +4,14 @@ height = int(input("What is your height in cm? "))
 # if statements
 if height >= 120:
     print("You can ride the rollercoaster!")
+    
+    age = int(input("What is your age? "))
+    if age <= 18:
+        print("Please pay $7")
+    elif 19 <= age <= 45:
+        print("Please pay $12")
+    else:
+        print("Please pay $20")
+    
 else:
     print("Sorry, you have to grow taller before you can ride")
