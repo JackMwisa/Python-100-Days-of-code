2f8d9d7356a66aec887cd906c2f3f2ce58ae7766
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2024-04-03 15:06:52 +0300

    Update one.py

--- Day 3.1/one.py
+++ Day 3.1/one.py
@@ -1,5 +1,6 @@
 print("Welcome to the rollercoaster")
 height = int(input("What is your height in cm? "))
+bill = 0
 
 # if statements
 if height >= 120:
@@ -7,11 +8,19 @@ if height >= 120:
     
     age = int(input("What is your age? "))
     if age <= 18:
+        bill = 7
         print("Please pay $7")
     elif 19 <= age <= 45:
+        bill = 12
         print("Please pay $12")
     else:
+        bill = 20
         print("Please pay $20")
-    
+
+    photo = input("Do you want a photo? Y or N: ")   
+    if photo.upper() == 'Y':
+        bill += 3
+        print(f"Your final bill is {bill}")
+
 else:
     print("Sorry, you have to grow taller before you can ride")
