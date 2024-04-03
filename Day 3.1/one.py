6bf6218956c0e9dc7c3420eede119ea41305acb5
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2024-04-03 15:09:16 +0300

    Update one.py

--- Day 3.1/one.py
+++ Day 3.1/one.py
@@ -1,12 +1,17 @@
 print("Welcome to the rollercoaster")
+# Ask the user for their height in centimeters
 height = int(input("What is your height in cm? "))
-bill = 0
+bill = 0  # Initialize the bill variable to keep track of the total cost
 
-# if statements
+# Check if the user's height is greater than or equal to 120 cm
 if height >= 120:
+    # If the height requirement is met, print a message allowing the user to ride
     print("You can ride the rollercoaster!")
     
+    # Ask the user for their age
     age = int(input("What is your age? "))
+    
+    # Determine the cost based on the user's age
     if age <= 18:
         bill = 7
         print("Please pay $7")
@@ -17,7 +22,10 @@ if height >= 120:
         bill = 20
         print("Please pay $20")
 
+    # Ask the user if they want a photo
     photo = input("Do you want a photo? Y or N: ")   
+    
+    # If the user wants a photo, add $3 to the bill
     if photo.upper() == 'Y':
         bill += 3
         print(f"Your final bill is {bill}")
