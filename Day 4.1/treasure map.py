594c7014381ef23d996fbbd29b05d82efaf23ed6
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2024-04-04 11:00:20 +0300

    Create treasure map.py

---
+++ Day 4.1/treasure map.py
@@ -0,0 +1,18 @@
+
+
+
+row1 = ["😆", "😆", "😆"]
+row2 = ["😭", "😭", "😭"]
+row3 = ["😣", "😣", "😣"]
+
+map = [row1,row2, row3]
+
+print(f"{row1}\n{row2}\n{row3}")
+
+position = input("where do you want to put the treasure? ")
+
+
+row = position[0]
+col = position[1]
+
+map[row][col] = "X"
