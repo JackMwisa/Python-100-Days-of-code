c1ce1061c2ea6131fb1511213cdc0ceb85b45574
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2024-04-04 10:37:12 +0300

    # Who is paying

--- Day 4.1/who_payin.py
+++ Day 4.1/who_payin.py
@@ -1,7 +1,14 @@
+import random
+
 # Who is paying
 
-name_string = input("Give me everybody's name separated by a comma. [,]")
+name_string = input("Give me everybody's name separated by a comma. [,] \n")
 
 name = name_string.split(", ")
 
 print(name)
+
+
+paying = name[random.randint(0,  len(name)-1)]  # Randomly select one of the names to pay
+
+print(f"{paying} is going to buy the meal today!")
