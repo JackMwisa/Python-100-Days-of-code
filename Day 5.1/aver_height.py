a764a37a09e2d8ca2097b61d9c69efaf731e407e
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2024-04-05 10:42:38 +0300

    average height challenge in a list using for loops

--- Day 5.1/aver_height.py
+++ Day 5.1/aver_height.py
@@ -7,3 +7,18 @@ for n in range(0, len(student_heights)):
 print(student_heights)
 
 
+
+summing = 0
+
+for summs in student_heights:
+    summing += summs #student_heights.index(summs)+1*summs
+    
+number = 0
+
+for students in student_heights:
+    number += 1
+    
+print(f"the total height is {summing}")
+print(f" the number of students is {number}")
+
+print(f" the average height of student is {summing // number}")
