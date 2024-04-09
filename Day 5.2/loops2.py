50ac46dc6c71d64854c048bacd7012ddbe378e36
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2024-04-09 15:19:50 +0300

    Update loops2.py

--- Day 5.2/loops2.py
+++ Day 5.2/loops2.py
@@ -1,4 +1,10 @@
-fruits = ['kiwi', 'orange', 'lemon']
-for i in fruits:
-    print(i)
-    print("Hello", i)
+# fruits = ['kiwi', 'orange', 'lemon']
+# for i in fruits:
+#     print(i)
+#     print("Hello", i)
+    
+
+
+for x in range(1, 100, 2):
+    if x % 2 == 0:
+        print(x)
