8c47d3cf185928d3909f08be17da2d4f41725765
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2024-04-12 12:26:44 +0300

    Update loops3.py

--- Day 5.2/loops3.py
+++ Day 5.2/loops3.py
@@ -3,3 +3,23 @@
 #     # print(x)
 #     if x % 2 != 0:
 #         print(x)
+
+
+
+row1 = ["😆", "😆", "😆"]
+row2 = ["😭", "😭", "😭"]
+row3 = ["😣", "😣", "😣"]
+
+map = [row1,row2, row3]
+
+print(f"{row1}\n{row2}\n{row3}")
+
+position = input("where do you want to put the treasure? ")
+
+
+row = int(position[0]) - 1
+col = int(position[1]) - 1
+
+map[row][col] = "X"
+
+print(f"{row1}\n{row2}\n{row3}")
