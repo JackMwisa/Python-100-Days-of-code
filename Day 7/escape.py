dc5068b8d6ca1568ad02b86d0414020eb4c58def
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2024-07-06 18:36:12 +0300

    Create escape.py

---
+++ Day 7/escape.py
@@ -0,0 +1,15 @@
+names_one = "\nHello\n "
+names_two = "\tI am okay!"
+names_three = "\'Heyyyy"
+names_four = "\"MOM\""
+names_five = "\\NNNN\\"
+
+course_name = "Python \"Progamming\""
+
+print(course_name)
+
+print(names_one)
+print(names_two)
+print(names_three)
+print(names_four)
+print(names_five)
