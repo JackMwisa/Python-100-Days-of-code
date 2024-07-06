9d553be6600ce1fb2fef418afa08b740a306ed38
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2024-07-06 18:30:32 +0300

    strings in python

---
+++ Day 7/strings.py
@@ -0,0 +1,31 @@
+course = "Python Programming"
+message = """
+Hi, John
+This is Carl, From UK.
+
+Regards
+Carl
+"""
+
+
+print(course)
+print(message)
+
+
+# len function
+print(len(course)) # length of something
+
+print(course[0])
+print(course[1])
+print(course[2])
+print(course[3])
+print(course[4])
+print(course[5]) #indexing
+
+print(course[-1])
+
+print(course[0:6]) #slicing
+
+print(course[:7])
+print(course[7:])
+print(course[:])
---
+++ Day 7/vars.py
@@ -0,0 +1,12 @@
+students_count = 1000
+rating = 4.98
+is_married = False
+his_name = "Joshua" + " " + 'Mukasa'
+course_name = '"Python Programming"'
+
+print("$")
+print(students_count)
+print(rating)
+print(is_married)
+print(his_name)
+print(course_name)
