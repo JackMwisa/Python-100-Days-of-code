b7c9a24dfefec58cfb3c5874e234fc0bd5593215
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2024-07-18 20:56:17 +0300

    Create squareroot.py

---
+++ Day 8/squareroot.py
@@ -0,0 +1,8 @@
+import math
+
+def hypotenuse(a, b):
+    a_squared = a ** 2
+    b_squared = b ** 2
+    sum_of_squares = a_squared + b_squared
+    hypotenuse_length = math.sqrt(sum_of_squares)
+    return hypotenuse_length
