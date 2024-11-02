c7fca4e2c120f3e1bb7562c0f8e56bf1d91cf0fc
Jack Mwisa <87465845+JackMwisa@users.noreply.github.com>
2024-11-02 16:22:04 +0300

    Update day-1-printing-start.py

--- Day 1/day-1-printing-start.py
+++ Day 1/day-1-printing-start.py
@@ -10,3 +10,24 @@ print("2. Knead the dough for 10 minutes.")
 print("3. Add 3g of Salt.")
 print("4. Leave to rise for 2 hours.")
 print("5. Bake at 200 degrees C for 30 minutes.")
+
+
+print("\nHello" + " " + "\nJack")
+
+
+# Answer part 1. Missing double quotes before the word Day.
+# print(Day 1 - String Manipulation")
+print("Day 1 - String Manipulation")
+
+
+# Answer part 2. Outer double quotes changed to single quotes.
+# print("String Concatenation is done with the "+" sign.")
+print('String Concatenation is done with the "+" sign.')
+
+# Answer part 3. Extra indentation removed
+#   print('e.g. print("Hello " + "world")')
+print('e.g. print("Hello " + "world")')
+
+# Answer part 4. Extra ( in print function removed.
+# print(("New lines can be created with a backslash and n.")
+print("New lines can be created with a backslash and n.")
